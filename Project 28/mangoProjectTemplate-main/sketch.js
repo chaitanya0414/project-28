770f4c65b6b89a70b898802cb578e3e0734e4baf
chaitanya0414 <72442976+chaitanya0414@users.noreply.github.com>
2021-02-09 22:03:28 +0530

    Add files via upload

---
+++ Project 28/mangoProjectTemplate-main/sketch.js
@@ -0,0 +1,46 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1 , mango2,mango3;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1100,150,30);
+	mango3=new mango(1100,180,30);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	stone=new Stone(200,300,110);
+	
+	slingshot = new Slingshot(boy.body,stone.body);
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  
+  groundObject.display();
+}
